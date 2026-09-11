a5ec62c99d0e25991e9a381807d7fdf99d24838f
Carebayo <sebasirl81@gmail.com>
2026-09-11 13:40:25 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const startButton = document.getElementById("comenzar");
 const story = document.getElementById("historia");
 const intro = document.querySelector(".intro");
+const annaSprite = document.getElementById("anna-sprite");
 
 startButton.addEventListener("click", function() {
 
@@ -11,5 +12,11 @@ startButton.addEventListener("click", function() {
     story.scrollIntoView({
         behavior: "smooth"
     });
+
+    setTimeout(() => {
+        if (annaSprite) {
+            annaSprite.classList.add("anna-visible");
+        }
+    }, 500);
 
 });
